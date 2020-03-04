8ae46f8d05eb0dc22380f2033e365a71a80f775f
thayari <alex.silvanovich@gmail.com>
2020-03-04 23:46:54 +0300

    fix

--- index.js
+++
@@ -1,2 +0,0 @@
-import './src/css/style.css';
-import './src/app';
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   devtool: 'source-map',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
